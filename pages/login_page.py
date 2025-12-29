fa4214e79d186becf36e112863c105f5fc1791cf
muhammaddanihaikal <dainnaxjakarta91@gmail.com>
2025-12-29 15:44:34 +0700

    Inisialisasi: Skrip otomasi Playwright LMS E-Campus

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page
+from config.config import BASE_URL
+
+class LoginPage:
+    def __init__(self, page: Page):
+        self.page = page
+
+        # Penanda elemen (locator)
+        self.username = page.get_by_role("textbox", name="Username")
+        self.password = page.get_by_role("textbox", name="Password")
+        self.login_btn = page.get_by_role("button", name="Login")
+
+    def open(self):
+        self.page.goto(f"{BASE_URL}/login")
+
+    def login(self, data):
+        self.username.fill(data["username"])
+        self.password.fill(data["password"])
+        self.login_btn.click()
